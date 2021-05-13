d938af1ec1a6e9c120c8316e0c1a3989df02b517
ystartgo <50304042+ystartgo@users.noreply.github.com>
2021-05-14 07:26:28 +0800

    Update page-timeline.php
    修正簡體中文轉繁體翻譯

--- user/page-timeline.php
+++ user/page-timeline.php
@@ -1,7 +1,7 @@
 <?php 
 
 /**
- Template Name: 时光轴模版
+ Template Name: 時光軸模版
  */
 
 get_header();
@@ -10,7 +10,7 @@ get_header();
 #ini_set('display_errors', '1');
 ?>
    	<div id="main">
-		<header class="page-header"><h1 class="cat-title">时光轴</h1> <span class="cat-des"><p>TimeLine</p> </span></header>
+		<header class="page-header"><h1 class="cat-title">時光軸</h1> <span class="cat-des"><p>TimeLine</p> </span></header>
         <div id="main-part">
 			<?php if (have_posts()) : the_post(); update_post_caches($posts); ?>
             <article class="art">
